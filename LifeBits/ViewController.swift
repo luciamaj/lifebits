1438a311ad18e5315ba71c30583a6cfa81f32c1e
Lucia <luciamaj@ied.edu>
2018-09-22 10:12:32 +0200

    Initial Commit

---
+++ LifeBits/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LifeBits
+//
+//  Created by lu on 22/09/18.
+//  Copyright © 2018 LuciaMaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
